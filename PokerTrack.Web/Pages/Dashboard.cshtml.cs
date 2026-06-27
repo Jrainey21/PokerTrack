c2c9cb89081890fe6b95848726411374ccd7a23f
joe <josephrainey21@gmail.com>
2026-06-27 13:34:29 -0400

    Added chart

--- PokerTrack.Web/Pages/Dashboard.cshtml.cs
+++ PokerTrack.Web/Pages/Dashboard.cshtml.cs
@@ -10,7 +10,8 @@ public class DashboardModel(SessionRepository repo) : PageModel
 {
     public SessionAnalytics? Analytics { get; private set; }
     public IEnumerable<PokerSession> RecentSessions { get; private set; } = [];
-
+    public string ChartLabelsJson { get; private set; } = "[]";
+    public string ChartDataJson { get; private set; } = "[]";
     public async Task OnGetAsync()
     {
         var userId = User.FindFirstValue("http://schemas.microsoft.com/identity/claims/objectidentifier")
@@ -21,5 +22,22 @@ public class DashboardModel(SessionRepository repo) : PageModel
         Analytics = await repo.GetAnalyticsAsync(userId);
         var all = await repo.GetByUserAsync(userId);
         RecentSessions = all.Take(10);
+
+        // Build cumulative profit data for the chart, oldest to newest
+        var chronological = all.OrderBy(s => s.SessionDate).ToList();
+        decimal runningTotal = 0;
+        var labels = new List<string>();
+        var data = new List<decimal>();
+
+        foreach (var s in chronological)
+        {
+            runningTotal += s.Profit;
+            labels.Add(s.SessionDate.ToString("MM/dd"));
+            data.Add(runningTotal);
+        }
+
+        ChartLabelsJson = System.Text.Json.JsonSerializer.Serialize(labels);
+        ChartDataJson = System.Text.Json.JsonSerializer.Serialize(data);
     }
+
 }
